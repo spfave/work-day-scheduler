71bcf97cdc59b842169cf69a0e66412fa9ddb531
Sebastian F <sebpfave2@gmail.com>
2021-03-16 22:32:09 -0400

    Demo schedule item text retrival

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 // DOM SELECTORS
+const schedule = $("#day-schedule");
 
 // FUNCTIONS
 // Get current date and display to header
@@ -6,6 +7,28 @@ const updateDate = () => {
   const today = moment().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(today);
 };
+
+// Update time block theme
+const updateTimeTheme = () => {};
+
+// Load saved schedule items from local storage
+const loadDaySchedule = () => {};
+
+// Save schedule item to local storage
+const saveScheduleItem = () => {
+  // localStorage.setItem();
+};
+
+// Handle schedule item save
+const handleSaveScheduleItem = (event) => {
+  // console.log($(event.target));
+
+  const textArea = $(event.target).parent().prev().children().first();
+  console.log(textArea.val());
+};
+
+// EVENT LISTENERS
+schedule.on("click", ".btn-save", handleSaveScheduleItem);
 
 // WEBPAGE EXECUTION
 // After page load
